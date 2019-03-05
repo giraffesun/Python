77298c44ddf02b57ec05d91611faf9609d24159c
sunpj <sunpeijian0592@qq.com>
2019-03-05 20:16:01 +0800

    efunds0305

--- src/Lession1/__init__.py
+++ src/Lession1/__init__.py
@@ -4,9 +4,9 @@ import MySQLdb
 conn = MySQLdb.connect(host='localhost', user='root',passwd='sun35741',charset="utf8")
 cursor = conn.cursor()
 count = cursor.execute('select * from jg_zjqs.bz_zxls') 
-print  '共有 %s条数据' % count 
+print('共有 %s条数据' %count)
 result = cursor.fetchone(); 
-print result
-print 'ID: %s info: %s' % (result[0],result[1])
+print (result)
+print ('ID: %s info: %s' % (result[0],result[1]))
 
 
